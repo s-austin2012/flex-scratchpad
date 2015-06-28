7af815f965347a937bc1c112ade89d68b51a5b58
red-hound <red-hound@users.noreply.github.com>
2015-06-28 12:47:23 -0400

    Create index.php
    index as php

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Coding Exercise</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- USFSP -->
+    <link rel='stylesheet' id='avia-google-webfont' href='//fonts.googleapis.com/css?family=PT+Sans%7COpen+Sans:400,600%7CEB+Garamond' type='text/css' media='all'/>
+    <link href="http://cook-my-catch.github.io/usfsp.css" rel="stylesheet">
+    <!-- end USFSP -->
+  </head>
+  <body>
+    <div class="container-fluid header">
+      <div class="col-md-12">
+    <header>
+      <h1>Coding Exercise</h1>
+      </header>
+      <nav>
+        <header><h3>Nav Menu</h3></header>
+        <ul>
+          <li>Source Documents</li>
+          <li>Validation</li>
+          <li>Illustrations</li>
+        </ul>
+      </nav>
+      </div><!-- closes md12 -->
+  </div><!-- closes container fluid -->
+  <div class="container-fluid">
+    <article>
+    <div class="col-md-12">
+      <header>
+        <h2>Source Documents</h2>
+      </header>
+      </div><!-- closes md12 for header -->
+      <div class="col-md-4">
+      <section>
+        <h3>Bootstrap</h3>
+        <h4><a href="http://getbootstrap.com/">Bootstrap Home Page</a></h4>
+        <h4><a href="https://github.com/twbs/bootstrap">Bootstrap on Github</a></h4>
+        <h4><a href="http://getbootstrap.com/getting-started/">Basic Bootstrap Template</a></h4>
+      </section>
+      </div><!-- closes colmd4 -->
+      <div class="col-md-4">
+      <section>
+      <header>
+          <h2>Validation References</h2>
+      </header>
+          <h3><a href="https://validator.w3.org/">W3C Validator</a></h3>
+      </section>
+      </div><!-- closes md4 -->
+      <div class="col-md-4">Facebook here</div>
+    </article>
+    </div><!-- closes container fluid -->
+    <article>
+      <header>
+        <h2>Illustrations</h2>
+      </header>
+      <section>
+        <figure>
+  <img src="http://www.fredcavazza.net/files/2009/09/html5_structure.png" alt="Example of HTML5 structuring tags" />
+  <figcaption>
+    <h5>Fig. 1 : an example of how new structuring elements could be used. This page put a &lt;nav&gt; on top, and does not have headers and footer for each article, like in this figure, but it could... By the way, this is a &lt;figcaption&gt; inside a &lt;figure&gt; element...</h5>
+  </figcaption>
+</figure>
+      
+      </section>
+        </article>
+    <aside style="width:100%"><!--Temporary override of Bootstrap style for aside-->
+      <h4>References</h4>
+      <ul>
+        <li><a href="https://validator.w3.org/">Nu Validator</a></li>
+        <li><a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Sections_and_Outlines_of_an_HTML5_document">Notes on Assistive Technology and Validation</a></li>
+        <li><a href="http://www.w3.org/html/wg/drafts/html/master/semantics.html#the-article-element">Each article should be identified, typically by including a heading (h1–h6 element) as a child of the article element.</a></li>
+      </ul>  
+        </aside>
+<footer>&nbsp;</footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+ 
