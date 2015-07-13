5811e976bc0981f87934ad72f273021795236cbb
red-hound <red-hound@users.noreply.github.com>
2015-07-13 12:30:23 -0400

    Create localstorage.js

---
+++ Forms/localstorage.js
@@ -0,0 +1,23 @@
+
+// Called when the page is loaded
+window.onload = restoreFormContent;
+
+function restoreFormContent() {
+  console.log("restoring form content from localStorage");
+  
+  if(localStorage.firstName !== undefined) 
+    document.getElementById("firstName").value = localStorage.firstName;
+ 
+  if(localStorage.lastName !== undefined) 
+    document.getElementById("lastName").value = localStorage.lastName;
+  
+  if(localStorage.email !== undefined) 
+    document.getElementById("email").value = localStorage.email;
+
+  if(localStorage.age !== undefined) 
+    document.getElementById("age").value = localStorage.age;
+  
+  if(localStorage.age !== undefined) 
+    document.getElementById("date").value = localStorage.date;
+
+}
