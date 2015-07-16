2db000c8ab01ab44365e78d66eacdaa378e803ac
red-hound <red-hound@users.noreply.github.com>
2015-07-16 09:06:41 -0400

    Create edx-coords1.js

---
+++ EDX/scripts/edx-coords1.js
@@ -0,0 +1,16 @@
+
+var displayCoords=document.getElementById("msg");
+  
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    displayCoords.innerHTML="Geolocation API not supported by your browser.";
+  }
+}
+  
+  
+function showPosition(position) {
+ displayCoords.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br />Longitude: " + position.coords.longitude;	
+}
